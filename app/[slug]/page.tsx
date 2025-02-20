7a93a5882379874c489d576af984e93bf9c0d2ad
zitons <zitons@outlook.com>
2025-02-20 17:47:33 +0800

    修复图片不可见

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -29,6 +29,16 @@ export default async function Blog({
     ).then((response) => response.json());
 
     const post: BlogPage = data.items[0];
+    function addTextToEmbedAlt(html: any, textToAdd: any, textToAdd2: any) {
+        // 使用正则表达式查找带 alt 属性的 embed 标签，并修改其 alt 属性
+        return html.replace(/(<embed[^>]*alt=")([^"]*)(")/g, (match: any, p1: any, p2: any, p3: any) => {
+            // 将 alt 的内容加上指定的 textToAdd 和 textToAdd2
+            const newAltValue = textToAdd + p2 + textToAdd2;
+
+            // 将 alt 改为 src，并且保持原有的值
+            return match.replace(`alt="${p2}"`, `src="${newAltValue}"`);
+        });
+    }
 
 
 
@@ -41,7 +51,7 @@ export default async function Blog({
                 </time>
                 <p className="my-4">{post.intro}</p>
             </div>
-            <div dangerouslySetInnerHTML={{ __html: post.body }}></div>
+            <div dangerouslySetInnerHTML={{ __html: addTextToEmbedAlt(post.body, 'http://137.184.36.245:8000/media/images/', '.original.png') }}></div>
         </main>
     );
 }
